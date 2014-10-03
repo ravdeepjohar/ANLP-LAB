6f1eea6690083e117d85c20fc880d30d8140bb92
Joe Bullard <jtb4478@cs.rit.edu>
2014-10-03 14:10:46 -0400

    Finished the functions I was originally delegated: number of tokens, number of non-dictionary tokens, and number of filled pauses. I generated my features into a CSV. Combinging CSVs will be a lot easier than combining code. i.e. in unix

--- joe.py
+++ joe.py
@@ -1,6 +1,46 @@
-FILLED = 'um uh uhhuh mm hmm hmhmm'.split()
+FILLED     = 'um uh uhhuh mm hmm hmhmm'.split()
+DICTIONARY = [ w.strip() for w in open('dictionary.txt', 'r') ]
+
+def num_tokens(transcript):
+    """Total number of word tokens
+    """
+    return sum(len(line.split()) for line in transcript)
 
 def num_filled_pauses(transcript):
     """Number of filled pauses (e.g. um, uh, er, hmm, etc.)
     """
-    return sum( w in FILLED for line in transcript for w in line.split() )
+    return sum(w in FILLED for line in transcript for w in line.split())
+
+def num_nondict_tokens(transcript):
+    """Number of word tokens not found in CMU dictionary
+    """
+    return sum(w not in DICTIONARY for line in transcript for w in line.split())
+
+FEATURES_JOE = [
+        num_tokens,
+        num_filled_pauses,
+        num_nondict_tokens,
+    ]
+
+
+################################################################################
+
+
+if __name__ == "__main__":
+    from sys import argv, exit
+    
+    if len(argv) < 2:
+        print 'usage: python2 %s [*.cha]+' % argv[0]
+        exit()
+
+    csv = [ '#' + ','.join(f.__name__ for f in FEATURES_JOE) + ',Label' ]
+
+    for cha in argv[1:]:
+        lines      = open(cha, 'r').readlines()
+        label      = lines[0].split()[-1]
+        transcript = lines[1:]
+        features   = [ f(transcript) for f in FEATURES_JOE ]
+        csv.append( ','.join([ str(f) for f in features ] + [label]) )
+
+    with open('features_joe.csv', 'w') as fout:
+        fout.write( '\n'.join(csv) + '\n' )
